78890fa9d28642fffbac7557b702a03ef93a5587
Daniel Stone <danielstone411@gmail.com>
2016-12-23 10:40:18 +0000

    Updated MainActivity in demo

--- app/src/main/java/com/danielstone/materialaboutlibrarydemo/MainActivity.java
+++ app/src/main/java/com/danielstone/materialaboutlibrarydemo/MainActivity.java
@@ -2,13 +2,12 @@ package com.danielstone.materialaboutlibrarydemo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,12 +18,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button) findViewById(R.id.example_button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ExampleMaterialAboutActivity.class);
+                startActivity(i);
             }
         });
 
