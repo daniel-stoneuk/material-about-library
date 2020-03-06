ce297ab068a3010273f6a30c0871030907463cd6
Nikolay Unuchek <n.unuchek@gramophone.by>
2020-03-06 09:40:01 +0300

    updated libs, updated styles to avoid crash of the app

--- app/src/main/java/com/danielstone/materialaboutlibrarydemo/Application.java
+++ app/src/main/java/com/danielstone/materialaboutlibrarydemo/Application.java
@@ -1,19 +1,10 @@
 package com.danielstone.materialaboutlibrarydemo;
 
 
-import com.squareup.leakcanary.LeakCanary;
-
 public class Application extends android.app.Application {
 
     @Override public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
-        // Normal app init code...
     }
 
 }
